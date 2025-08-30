88fd7f1aa3f82036b58ac9ae1897166183491a7d
thiagoeu <araujo.thiago1051@gmail.com>
2025-08-30 18:12:26 -0300

    feat: users simple crud + dbconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+import { databaseConfig } from './config/db.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(databaseConfig), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/db.config.ts
@@ -0,0 +1,13 @@
+// database.config.ts
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'farmacia-popular',
+  autoLoadEntities: true,
+  synchronize: true,
+};
