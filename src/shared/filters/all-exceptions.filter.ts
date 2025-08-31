8bf9d999e1b7adcd0c762df5f74599003472df9e
thiagoeu <araujo.thiago1051@gmail.com>
2025-08-30 23:52:04 -0300

    feat: users CRUD + enums + tratamento de erro

---
+++ src/shared/filters/all-exceptions.filter.ts
@@ -0,0 +1,34 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch() // captura qualquer exceção
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal server error';
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const exceptionResponse: any = exception.getResponse();
+
+      message =
+        (exceptionResponse.message as string) || exception.message || message;
+    }
+
+    response.status(status).json({
+      success: false,
+      statusCode: status,
+      message,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
