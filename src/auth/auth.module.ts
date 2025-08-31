5c6d60cd24b6dd8ba67bc125ccf534480bb2b358
thiagoeu <araujo.thiago1051@gmail.com>
2025-08-31 01:14:04 -0300

    fix: correção dos pacotes + modulo auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
 export class AuthModule {}
