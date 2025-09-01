582b502b17cea9c03cd1950d8794ebe8930b28eb
thiagoeu <araujo.thiago1051@gmail.com>
2025-09-01 15:36:53 -0300

    refact: ajuste service hello

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const apiDoc = 'http://localhost:3000/api';
+    return `Documentação: <a href="${apiDoc}">${apiDoc}</a>`;
   }
 }
