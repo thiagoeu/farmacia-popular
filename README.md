731875839fa59ec02736b84e23092d69756c5713
thiagoeu <araujo.thiago1051@gmail.com>
2025-09-01 10:49:04 -0300

    refact: ajustes de segurança adicionado variaveis de ambiente

--- README.md
+++ README.md
@@ -113,7 +113,7 @@ As configurações de conexão estão definidas no arquivo `src/config/db.config
 ```env
 DATABASE_HOST=localhost
 DATABASE_PORT=5432
-DATABASE_USER=postgres
+DATABASE_USERNAME=postgres
 DATABASE_PASSWORD=postgres
 DATABASE_NAME=farmacia-popular
 
--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,11 +3,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'farmacia-popular',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: Number(process.env.DATABASE_PORT) || 5432,
+  username: process.env.DATABASE_USERNAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  database: process.env.DATABASE_NAME || 'farmacia-popular',
   autoLoadEntities: true,
   synchronize: true,
 };
