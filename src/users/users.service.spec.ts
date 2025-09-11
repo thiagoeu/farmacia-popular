c431d144819f18ee2d0c7e3444e13304d1e8d61c
thiagoeu <araujo.thiago1051@gmail.com>
2025-09-11 11:58:34 -0300

    feat: teste unitarios

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepository: {
+    findOneBy: jest.Mock;
+    create: jest.Mock;
+    save: jest.Mock;
+  };
 
   beforeEach(async () => {
+    userRepository = {
+      findOneBy: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: userRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
+  // Cria um novo user
+  it('should create a user', async () => {
+    userRepository.findOneBy.mockResolvedValue(null); // simulando que o user não existe
+    userRepository.create.mockImplementation((dto) => dto);
+    userRepository.save.mockImplementation((dto) => ({
+      id: 1,
+      ...dto,
+    }));
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+    const user = await service.create({
+      name: 'John Doe',
+      email: 'xgV0t@example.com',
+      password: '123456',
+    });
+
+    expect(userRepository.findOneBy).toHaveBeenCalledWith({
+      email: 'xgV0t@example.com',
+    });
+    expect(userRepository.save).toHaveBeenCalled();
+    expect(user.password).not.toBe('123456'); // senha deve estar criptografada
+    expect(user).toHaveProperty('id');
+    expect(user).toHaveProperty('email', 'xgV0t@example.com');
   });
 });
